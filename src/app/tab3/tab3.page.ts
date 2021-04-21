4e88f9b2e7078e7ccb88a1f378b49c4a093b1458
RoseMarie1326 <narbonitarosemarie99@gmail.com>
2021-04-21 15:52:02 +0800

    Commit files

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,53 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  contactname="";
+  contactnumber="";
+  contacts: any = [];
+
+
+  constructor(public alertCtrl: AlertController)  {}
+
+  save() {
+    let contact = {
+      name: this.contactname,
+      number: this.contactnumber
+
+    }
+    this.contacts.push(contact);
+    console.log(this.contacts);
+    this.clearField();
+
+  }
+  clearField() {
+    this.contactname = "";
+    this.contactnumber = "";
+  }
+  
+
+  async delete(cont) {
+    const confirm = await this.alertCtrl.create({
+      header: 'Delete!',
+      message: 'Are you sure you want to delete this contact?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+          }
+        }, {
+          text: 'Delete',
+          handler: () => {
+            let index = this.contacts.indexOf(cont);
+            if(index > -1){
+              this.contacts.splice(index,1);
+            }
+        }
+        }
+      ]
+    } );
+
+    await confirm.present();
+  }
 
 }
