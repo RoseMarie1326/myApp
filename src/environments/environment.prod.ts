68aed91edef3d6c429ae0522b13706c91460b962
RoseMarie1326 <narbonitarosemarie99@gmail.com>
2021-08-06 21:13:28 +0800

    Commit all files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true, 
+firebaseConfig : {
+  apiKey: "AIzaSyA3CseDOALAIUWgI5XwC2SBZ_8guIEX4r8",
+  authDomain: "myapp-e8947.firebaseapp.com",
+  databaseURL: "https://myapp-e8947-default-rtdb.firebaseio.com",
+  projectId: "myapp-e8947",
+  storageBucket: "myapp-e8947.appspot.com",
+  messagingSenderId: "872547984459",
+  appId: "1:872547984459:web:a20c1e6c158e3faee899ce",
+  measurementId: "G-TV3YSHFD18"
+}
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+firebaseConfig : {
+  apiKey: "AIzaSyA3CseDOALAIUWgI5XwC2SBZ_8guIEX4r8",
+  authDomain: "myapp-e8947.firebaseapp.com",
+  databaseURL: "https://myapp-e8947-default-rtdb.firebaseio.com",
+  projectId: "myapp-e8947",
+  storageBucket: "myapp-e8947.appspot.com",
+  messagingSenderId: "872547984459",
+  appId: "1:872547984459:web:a20c1e6c158e3faee899ce",
+  measurementId: "G-TV3YSHFD18"
+}
+
 };
 
 /*
